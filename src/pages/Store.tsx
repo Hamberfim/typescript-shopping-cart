8e0038bbd737cfc50226ccc6e3ce28aa2bae5775
Anthony Hamlin <adhamlingithub@gmail.com>
2022-06-21 17:28:38 -0500

    json data mock, punlic img files, format currency util, prod card layout

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 export function Store() {
-  return <h1>Store Page</h1>;
+  return (
+    <>
+      <h1>Store Page</h1>
+      <Row xs={1} md={2} lg={4} className="g-2">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
