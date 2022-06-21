eaab4d698c0965e94ba7c6729f03a39543f0fb3f
Anthony Hamlin <adhamlingithub@gmail.com>
2022-06-21 12:20:07 -0500

    route pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
 
 function App() {
   return (
-    <Container>
-      <h2>HELLO | Placeholder</h2>
+    <Container className="mb-4">
+      <Routes>
+        {/* navigation routes - pages */}
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Container>
   );
 }
