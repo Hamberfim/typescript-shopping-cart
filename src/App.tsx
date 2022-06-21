9fccae449cd11fb5e1360c4e798f54c6d0f239a7
Anthony Hamlin <adhamlingithub@gmail.com>
2022-06-21 12:48:40 -0500

    built nav, cart button | stop: https://youtu.be/lATafp15HWA?t=931

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { Container } from "react-bootstrap";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <Container className="mb-4">
-      <Routes>
-        {/* navigation routes - pages */}
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
+    /* tage fragment so it will render everything to the screen */
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          {/* navigation routes - pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
