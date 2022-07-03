467a985235bed45e96ffc79d7e0613e7063b6da9
Anthony <33159808+Hamberfim@users.noreply.github.com>
2022-07-03 11:50:51 -0500

    Update About.tsx
    typo

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -20,7 +20,7 @@ export function About() {
             <div>
               <p>
                 The Shopping Corporation is a high-tech logistical firm
-                primarily focused on the production of low quality product as
+                primarily focused on the production of low quality product at
                 insanely cheep prices. We are based in Los Angeles and named
                 after its founder John Shopping.{" "}
               </p>
